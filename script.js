cb06928c1308546ef52fbf3ed8802a177379ed81
Saurabh Khedkar <70061591+khedkar2001@users.noreply.github.com>
2025-05-30 18:35:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const line = document.getElementById('line');
 let angle = 0;
 
+// Rotate by 2 degrees every 20ms
 setInterval(() => {
   angle += 2;
   line.style.transform = `translate(-50%, -50%) rotate(${angle}deg)`;
